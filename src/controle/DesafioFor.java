68754d6a1ed94e176c2744baea7c2dfead026cc6
Varyon <felipedcmv_@hotmail.com>
2026-02-20 04:54:13 -0300

    commit (controls - For)

---
+++ src/controle/DesafioFor.java
@@ -0,0 +1,9 @@
+package controle;
+
+public class DesafioFor {
+    public static void main(String[] args) {
+        for(String value = "#"; !value.equals("######"); value += "#"){
+            System.out.println(value);
+        }
+    }
+}
