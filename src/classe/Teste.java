74215b28e1338f11b90dd03c8d6f818b95c152ee
Varyon <felipedcmv_@hotmail.com>
2026-02-22 18:24:14 -0300

    commit (class)

---
+++ src/classe/Teste.java
@@ -0,0 +1,10 @@
+package classe;
+
+public class Teste {
+    int a = 3;
+    public static void main(String[] args) {
+
+        Teste teste = new Teste();
+        System.out.println(teste.a);
+    }
+}
