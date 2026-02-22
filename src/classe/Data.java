1185b39956048be7b45deca8d98ee26576b18cbf
Varyon <felipedcmv_@hotmail.com>
2026-02-22 16:44:35 -0300

    commit (class - constructor)

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -4,6 +4,18 @@ public class Data {
 
     int day, month, year;
 
+    Data(){
+        this.day = 1;
+        this.month = 1;
+        this.year = 1970;
+    }
+
+    Data(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
     String formattedDate (){
         return this.day + "/" + this.month + "/" + this.year;
     }
--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -4,16 +4,10 @@ public class DataTeste {
     public static void main(String[] args) {
 
         Data data1 = new Data();
-        data1.day = 2;
-        data1.month = 10;
-        data1.year = 1997;
 
         System.out.println(data1.formattedDate());
 
-        Data data2 = new Data();
-        data2.day = 6;
-        data2.month = 10;
-        data2.year = 2018;
+        Data data2 = new Data(2, 10, 1997);
 
         System.out.println(data2.formattedDate());
 
